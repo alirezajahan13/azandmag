f2bbc217e2ceb95eac15b75230e4d1b0bf0df122
mahla-mohamadi <mahlamhmd78@gmail.com>
2024-01-08 13:54:36 +0330

    sourcecom

---
+++ page-templates/source-template.php
@@ -0,0 +1,120 @@
+<?php 
+
+/*
+
+Template Name: Source Template
+
+*/
+
+get_header();
+
+?>
+
+<div class="mainView">
+    <h1>هدینگ یک</h1>
+
+    <h2>هدینگ دو</h2>
+
+    <h3>هدینگ سه</h3>
+
+    <h4>هدینگ چهار</h4>
+
+    <h5>هدینگ پنج</h5>
+
+    <h6>هدینگ شش</h6>
+
+    <div class="generalHeading">
+        <h2>هدینگ جنرال صفحه</h2>
+    </div>
+    
+    <div class="generalHeading bottomBorder">
+        <h2>هدینگ جنرال صفحه با زیر هدینگ</h2>
+        <span>این یک زیر هدینگ برای یک هدینگ جنرال صفحه است</span>
+    </div>
+
+    <h3>استایل نمایش دکمه لینک</h3>
+
+    <div class="mainBoxShadow highPadding mediumMargined">
+        <h3>این باکس دارای سایه جنرال وبسایت است</h3>
+        <a class="generalButton" href="#">مشاهده</a>
+        <a class="generalButton fillButton" href="#">مشاهده</a>
+        <a class="generalButton fillButton fillButtonNoArrow" href="#">مشاهده</a>
+        <a class="generalButton noArrowButton" href="#">مشاهده</a>
+        <button class="generalButton fillButton">مشاهده</button>
+        <button class="generalButton fillButton fillButtonNoArrow">مشاهده</button>
+        <button class="generalButton noArrowButton">مشاهده</button>
+    </div>
+    <div class="mainBoxBorder highPadding mediumMargined">
+        <h3>باکس بردر دار جنرال</h3>
+        
+        <li><a href="#">لیست با لینک</a></li>
+
+        <li class="noListStyle"><a href="">لیست با لینک بدون علامت</a></li>
+
+        <li class="darkListStyle">لیست معمولی تیره</li>
+
+        <li class="checkListStyle">لیست با آیکن چک</li>
+
+        <li class="crossListStyle">لیست با آیکن کراس</li>
+
+        <li class="arrowListStyle">لیست با آیکن فلش</li>
+    </div>
+
+    <!-- <h3 class="mediumWeight">تست تیتر سبک</h3>
+
+
+    <p>
+
+    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+
+    </p>
+
+
+    <table>
+
+        <tr><th>لورم ایپسوم</th><th>لورم ایپسوم</th><th>لورم ایپسوم</th><th>لورم ایپسوم</th><th>لورم ایپسوم</th></tr>
+
+        <tr><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td></tr>
+
+        <tr><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td></tr>
+
+        <tr><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td></tr>
+
+        <tr><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td><td>لورم ایپسوم متن ساختگی </td></tr>
+
+    </table>
+
+    <br>
+
+
+    <h3>فرم‌ها</h3>
+
+    <form action="">
+
+        <label>نام و نام خانوادگی<input placeholder="نام شما" type="text" name="" id=""></label>
+
+        <label>ایمیل<input placeholder="نام شما" type="text" name="" id=""></label>
+
+        <label>شماره تماس<input placeholder="نام شما" type="text" name="" id=""></label>
+
+        <label>رمز<input placeholder="نام شما" type="text" name="" id=""></label>
+
+        <label>توضیحات<textarea placeholder="نام شما" name="" id="" cols="30" rows="10"></textarea></label>
+
+        <input type="submit" value="ثبت درخواست">
+
+    </form>
+
+
+
+    <div class="siteMessage warningMessage">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</div>
+
+    <div class="siteMessage successMessage">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</div>
+
+    <div class="siteMessage errorMessage">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</div>
+
+    <div class="siteMessage informMessage">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</div> -->
+
+</div>
+
+<?php get_footer();
