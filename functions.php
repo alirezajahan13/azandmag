118b756de699613073ae5a59ef3230ca11d9a8b0
mahla-mohamadi <mahlamhmd78@gmail.com>
2024-01-09 14:37:53 +0330

    homepagepostscom

--- functions.php
+++ functions.php
@@ -141,11 +141,11 @@ function azandmag_scripts() {
 	wp_enqueue_style( 'azandmag-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'azandmag-style', 'rtl', 'replace' );
 	wp_enqueue_style( 'additional-style', get_template_directory_uri().'/additional.css', array(), _S_VERSION );
-	wp_enqueue_style( 'swiper-bundle', get_stylesheet_uri(),'/swiper/swiper-bundle.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'swiper-style', get_template_directory_uri(). '/swiper/swiper-bundle.min.css' , array(), _S_VERSION );
 
 	wp_enqueue_script('jquery');
 	wp_enqueue_script( 'azandmag-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script( 'swiper-bundle-min', get_template_directory_uri() . '/swiper/swiper-bundle.min.js', array('jquery'), _S_VERSION, true );
+	wp_enqueue_script( 'swiper-js', get_template_directory_uri() . '/swiper/swiper-bundle.min.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'azandswiper', get_template_directory_uri() . '/js/azandsw.js', array('jquery'), _S_VERSION, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
