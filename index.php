368d7a60426a849dbe6f81452a160373e78c6a9a
mahla-mohamadi <mahlamhmd78@gmail.com>
2024-01-10 18:47:37 +0330

    transformcom

--- index.php
+++ index.php
@@ -15,19 +15,22 @@
 get_header();
 ?>
 
-<div class="azandBlogPageIntroContainer">
-	<a href="#" class="blogPageIntroCard">
-		<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
-		<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
-	</a>
-	<a href="#" class="blogPageIntroCard">
-		<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
-		<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
-	</a>
-	<a href="#" class="blogPageIntroCard">
-		<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
-		<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
-	</a>
+<div class="azandBlogPageIntroContainer swiper mySwiperSlider">
+	<div class="changeClassToSwiperWraper swiper-wrapper">
+		<a href="#" class="blogPageIntroCard swiper-slide">
+			<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
+			<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
+		</a>
+		<a href="#" class="blogPageIntroCard swiper-slide">
+			<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
+			<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
+		</a>
+		<a href="#" class="blogPageIntroCard swiper-slide">
+			<img src="<?php bloginfo('template_directory') ?>/img/2.jpeg" alt="">
+			<h2>لورم ایپسوم متن ساختگی با تولید سادگی</h2>
+		</a>
+	</div>
+	<div class="swiper-scrollbar"></div>
 </div>
 <div class="azandBlogFeaturedContentContainer sectionPadding mainView">
 	<div class="generalHeading ">
@@ -80,106 +83,134 @@ get_header();
 <div class="gridSectionBlogParent">
 	<div class="gridSectionBlog mainView">
 		<div class="recentPostsHomeListRight">
-		<div class="generalHeading">
-			<h2>پربازدید ترین مطالب</h2>
-		</div>
-		<?php
-            $recent_posts_args = array(
-                'posts_per_page' => 3,
-                'orderby' => 'date',
-                'order' => 'DESC',
-            );
-            $recent_posts_query = new WP_Query($recent_posts_args);
-            if ($recent_posts_query->have_posts()) {
-                echo '<div class="recentPostsHomeList">';
-                // Loop through the recent posts
-                while ($recent_posts_query->have_posts()) {
-                    $recent_posts_query->the_post();
-                    ?>
-                    <a href="<?php the_permalink(); ?>" class="recent-post">
-                        <?php if (has_post_thumbnail()) : ?>
-                            <?php the_post_thumbnail('full'); ?>
-                        <?php endif; ?>
-						<div class="detailBoxsParent">
-							<h3><?php the_title(); ?></h3>
-							<div class="detailBoxsFooter">
-								<div class="timeCountSinglePost">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="12" height="12" fill="#fff"><path d="M8 0c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8 3.6-8 8-8zm0 2C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm1 2v3h3v2H7V4h2z"/></svg>
-									<span>مطالعه '<?php echo do_shortcode('[reading_time]') ?></span>
-								</div>
-								<div class="homePageBlogCardTimeCount">
-									<span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" version="1.1" fill="#fff"><path d="M 15 2 L 13 2 L 13 0 L 10 0 L 10 2 L 6 2 L 6 0 L 3 0 L 3 2 L 1 2 C 0.449219 2 0 2.449219 0 3 L 0 15 C 0 15.550781 0.449219 16 1 16 L 15 16 C 15.550781 16 16 15.550781 16 15 L 16 3 C 16 2.449219 15.550781 2 15 2 Z M 14 14 L 2 14 L 2 5 L 14 5 Z M 14 14 "></path></svg></span>
-									<span><?php echo get_the_date('j F') ?></span>
-								</div>
-							</div>
-						</div>
-                    </a>
-                    <?php
-                }
-                echo '</div>';
-            }
-            wp_reset_postdata();
-        ?>
-		<div class="generalHeading">
-			<h2>پربازدید ترین مطالب</h2>
-		</div>
-		<?php
-            $recent_posts_args = array(
-                'posts_per_page' => 3,
-                'meta_key' => 'views',
-				'orderby' => 'meta_value_num',
-				'post_status' => 'publish',
-				'order'=>'DESC',
-            );
-            $recent_posts_query = new WP_Query($recent_posts_args);
-            if ($recent_posts_query->have_posts()) {
-                echo '<div class="recentPostsHomeList">';
-                // Loop through the recent posts
-                while ($recent_posts_query->have_posts()) {
-                    $recent_posts_query->the_post();
-                    ?>
-                    <a href="<?php the_permalink(); ?>" class="recent-post">
-                        <?php if (has_post_thumbnail()) : ?>
-                            <?php the_post_thumbnail('full'); ?>
-                        <?php endif; ?>
-						<div class="detailBoxsParent">
-							<h3><?php the_title(); ?></h3>
-							<div class="detailBoxsFooter">
-								<div class="timeCountSinglePost">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="12" height="12" fill="#fff"><path d="M8 0c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8 3.6-8 8-8zm0 2C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm1 2v3h3v2H7V4h2z"/></svg>
-									<span>مطالعه '<?php echo do_shortcode('[reading_time]') ?></span>
-								</div>
-								<div class="homePageBlogCardTimeCount">
-									<span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" version="1.1" fill="#fff"><path d="M 15 2 L 13 2 L 13 0 L 10 0 L 10 2 L 6 2 L 6 0 L 3 0 L 3 2 L 1 2 C 0.449219 2 0 2.449219 0 3 L 0 15 C 0 15.550781 0.449219 16 1 16 L 15 16 C 15.550781 16 16 15.550781 16 15 L 16 3 C 16 2.449219 15.550781 2 15 2 Z M 14 14 L 2 14 L 2 5 L 14 5 Z M 14 14 "></path></svg></span>
-									<span><?php echo get_the_date('j F') ?></span>
+		<div class="homeGridOne">
+			<div class="generalHeading">
+				<h2>پربازدید ترین مطالب</h2>
+			</div>
+			<?php
+				$recent_posts_args = array(
+					'posts_per_page' => 3,
+					'orderby' => 'date',
+					'order' => 'DESC',
+				);
+				$recent_posts_query = new WP_Query($recent_posts_args);
+				if ($recent_posts_query->have_posts()) {
+					echo '<div class="recentPostsHomeList">';
+					// Loop through the recent posts
+					while ($recent_posts_query->have_posts()) {
+						$recent_posts_query->the_post();
+						?>
+						<a href="<?php the_permalink(); ?>" class="recent-post">
+							<?php if (has_post_thumbnail()) : ?>
+								<?php the_post_thumbnail('full'); ?>
+							<?php endif; ?>
+							<div class="detailBoxsParent">
+								<h3><?php the_title(); ?></h3>
+								<div class="detailBoxsFooter">
+									<div class="timeCountSinglePost">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="12" height="12" fill="#fff"><path d="M8 0c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8 3.6-8 8-8zm0 2C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm1 2v3h3v2H7V4h2z"/></svg>
+										<span>مطالعه '<?php echo do_shortcode('[reading_time]') ?></span>
+									</div>
+									<div class="homePageBlogCardTimeCount">
+										<span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" version="1.1" fill="#fff"><path d="M 15 2 L 13 2 L 13 0 L 10 0 L 10 2 L 6 2 L 6 0 L 3 0 L 3 2 L 1 2 C 0.449219 2 0 2.449219 0 3 L 0 15 C 0 15.550781 0.449219 16 1 16 L 15 16 C 15.550781 16 16 15.550781 16 15 L 16 3 C 16 2.449219 15.550781 2 15 2 Z M 14 14 L 2 14 L 2 5 L 14 5 Z M 14 14 "></path></svg></span>
+										<span><?php echo get_the_date('j F') ?></span>
+									</div>
 								</div>
 							</div>
-						</div>
-                    </a>
-                    <?php
-                }
-                echo '</div>';
-            }
-            wp_reset_postdata();
-        ?>
+						</a>
+						<?php
+					}
+					echo '</div>';
+				}
+				wp_reset_postdata();
+			?>
+		</div>
+		<div class="homeHotTags">
+			<div class="generalHeading">
+				<h2>برچسب های داغ روز</h2>
+			</div>
+			<div class="blogItemsTags mainBoxBorder">
+				<?php
+					$tags = get_terms( array(
+						'taxonomy' => 'post_tag',
+						'hide_empty' => true,
+					) );
+
+					foreach ($tags as $tag) {
+						echo '<div class="itemsTagParent">';
+						echo '<a class="blogTagItem generalButton noArrowButton" href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>';
+						echo '</div>';
+					}
+				?>
+			</div>
+		</div>
+		<div class="homeGridTwo">
+			<div class="generalHeading">
+				<h2>پربازدید ترین مطالب</h2>
+			</div>
+			<?php
+				$recent_posts_args = array(
+					'posts_per_page' => 3,
+					'meta_key' => 'views',
+					'orderby' => 'meta_value_num',
+					'post_status' => 'publish',
+					'order'=>'DESC',
+				);
+				$recent_posts_query = new WP_Query($recent_posts_args);
+				if ($recent_posts_query->have_posts()) {
+					echo '<div class="recentPostsHomeList">';
+					// Loop through the recent posts
+					while ($recent_posts_query->have_posts()) {
+						$recent_posts_query->the_post();
+						?>
+						<a href="<?php the_permalink(); ?>" class="recent-post">
+							<?php if (has_post_thumbnail()) : ?>
+								<?php the_post_thumbnail('full'); ?>
+							<?php endif; ?>
+							<div class="detailBoxsParent">
+								<h3><?php the_title(); ?></h3>
+								<div class="detailBoxsFooter">
+									<div class="timeCountSinglePost">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="12" height="12" fill="#fff"><path d="M8 0c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8 3.6-8 8-8zm0 2C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm1 2v3h3v2H7V4h2z"/></svg>
+										<span>مطالعه '<?php echo do_shortcode('[reading_time]') ?></span>
+									</div>
+									<div class="homePageBlogCardTimeCount">
+										<span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" version="1.1" fill="#fff"><path d="M 15 2 L 13 2 L 13 0 L 10 0 L 10 2 L 6 2 L 6 0 L 3 0 L 3 2 L 1 2 C 0.449219 2 0 2.449219 0 3 L 0 15 C 0 15.550781 0.449219 16 1 16 L 15 16 C 15.550781 16 16 15.550781 16 15 L 16 3 C 16 2.449219 15.550781 2 15 2 Z M 14 14 L 2 14 L 2 5 L 14 5 Z M 14 14 "></path></svg></span>
+										<span><?php echo get_the_date('j F') ?></span>
+									</div>
+								</div>
+							</div>
+						</a>
+						<?php
+					}
+					echo '</div>';
+				}
+				wp_reset_postdata();
+			?>
+		</div>
 		<div class="blogItemsCategoryParent">
 			<div class="generalHeading">
 				<h2>دسته بندی ها</h2>
 			</div>
-			<div class="blogItemsCategory">
-				<?php
-					$categories = get_terms( array(
-						'taxonomy' => 'category',
-						'hide_empty' => true,
-						'exclude'=>array('1')
-					) );
-					foreach($categories as $category) {
-						echo '<div class="itemsCatParent">';
-						echo '<svg width="20" height="20" fill="#9696a0" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)"><path d="M0 0h48v48H0z" fill="none"/><path d="M8 26h25.172l-14 14L22 42.828 40.828 24 22 5.172 19.172 8l14 14H8z"/></svg>';
-						echo '<a class="blogCatItem" href="' . get_category_link($category->term_id) . '">' . $category->name . '</a>';
-						echo '</div>';
-					}
-				?>
+			<div class="blogItemsCategory mainBoxBorder">
+			<?php
+				$categories = get_terms( array(
+					'taxonomy' => 'category',
+					'hide_empty' => true,
+					'exclude' => array('1')
+				) );
+
+				foreach ($categories as $category) {
+					// Get the category count
+					$category_count = $category->count;
+
+					echo '<a class="itemsCatParent" href="' . get_category_link($category->term_id) . '">';
+					echo '<span class="blogCatItem" >' . $category->name . '</span>';
+					// Display the category count
+					echo '<span class="categoryCount">' . $category_count . ' مطلب</span>';
+					echo '</a>';
+				}
+			?>
 			</div>
 		</div>
 		</div>
@@ -188,6 +219,7 @@ get_header();
 			<h2>آخرین مطالب</h2>
 		</div>
 		<?php
+			echo '<div class="innerRecentPostsHomeListLeft">';
 				$latestargs = array(
 					'post_type'=>'post',
 					'posts_per_page' => -1,
@@ -217,7 +249,9 @@ get_header();
 					endwhile;
 					// Reset Post Data
 					wp_reset_postdata();
+					echo '</div>';
 				?>
+			
 		</div>
 	</div>
 </div>
